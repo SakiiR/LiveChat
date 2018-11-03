a10d14d72c775c2d0579f1f96663e42e0061aca5
SakiiR SakiiR <sakiirlessong@gmail.com>
2018-11-03 13:47:56 -0700

    Added theme

--- front/src/index.js
+++ front/src/index.js
@@ -3,17 +3,30 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import { Provider } from "react-redux";
 import { Router } from "react-router-dom";
+import pink from "@material-ui/core/colors/pink";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import App from "./containers/App/App";
 import registerServiceWorker from "./registerServiceWorker";
 import store from "./redux/store";
 import history from "./utils/history";
 
+const theme = createMuiTheme({
+  palette: {
+    primary: pink,
+    secondary: {
+      main: "#f06292"
+    }
+  }
+});
+
 ReactDOM.render(
-  <Provider store={store}>
-    <Router history={history}>
-      <App />
-    </Router>
-  </Provider>,
+  <MuiThemeProvider theme={theme}>
+    <Provider store={store}>
+      <Router history={history}>
+        <App />
+      </Router>
+    </Provider>
+  </MuiThemeProvider>,
   document.getElementById("root")
 );
 registerServiceWorker();
